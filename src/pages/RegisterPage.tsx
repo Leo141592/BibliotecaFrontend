3d50246e9c44cb2ccd38b8959a323d3e8b1f8fbd
Leo141592 <her25041@uvg.edu.gt>
2026-05-26 15:08:31 -0600

    Pagina crear cuenta

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom"
-function LoginPage() {
+function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-96">
         <h1 className="text-3xl font-bold text-center mb-6">
-          Iniciar Sesión
+          Crear Cuenta
         </h1>
 
         <form className="flex flex-col gap-4">
           <input
             type="text"
             placeholder="Nombre de usuario"
+            className="border p-3 rounded-lg"
+          />
+
+          <input
+            type="email"
+            placeholder="Correo electrónico"
             className="border p-3 rounded-lg"
           />
 
@@ -22,24 +27,14 @@ function LoginPage() {
 
           <button
             type="submit"
-            className="bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition"
+            className="bg-green-600 text-white p-3 rounded-lg hover:bg-green-700 transition"
           >
-            Entrar
+            Crear Cuenta
           </button>
         </form>
-
-        <p className="text-center mt-4">
-          ¿No tienes cuenta?{" "}
-          <Link
-            to="/register"
-            className="text-blue-600 cursor-pointer"
-            >
-            Crear cuenta
-        </Link>
-        </p>
       </div>
     </div>
   )
 }
 
-export default LoginPage
+export default RegisterPage
