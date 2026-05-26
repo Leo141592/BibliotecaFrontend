35d20f02ba320b55c824307b0874552522559de0
Leo141592 <her25041@uvg.edu.gt>
2026-05-26 14:18:19 -0600

    Setup react y vite

---
+++ src/data/mockBooks.ts
@@ -0,0 +1,14 @@
+export const mockBooks = [
+  {
+    id: 1,
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian"
+  },
+  {
+    id: 2,
+    title: "Dune",
+    author: "Frank Herbert",
+    genre: "Sci-Fi"
+  }
+]
